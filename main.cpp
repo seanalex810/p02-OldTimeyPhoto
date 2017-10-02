f3daba513a18e8e8a1ec48db3ec5a40f40df8211
seanalex810 <31226161+seanalex810@users.noreply.github.com>
2017-10-02 16:38:09 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<string> 
+#include<vector> 
+#include"bitmap.h" 
+
+int main()
+{
+return 0; 
+} 
+// 
+prompt user to enter in image
+correct for user input error 
+use a loop to test all the images pixels and find the average value of the 3 components in the pixel then assign to that average value.
