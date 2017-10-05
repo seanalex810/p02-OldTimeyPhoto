c4bf7afc29f89d2ebbbd9af0288b0f125a9b5384
seanalex810 <salex@mail.csuchico.edu>
2017-10-04 22:05:47 -0700

    10/4 p02

--- main.cpp
+++ main.cpp
@@ -2,11 +2,18 @@
 #include<vector> 
 #include"bitmap.h" 
 
+using namespace std;
 int main()
 {
+Bitmap image; 
+vector<vector <Pixel> >machupicchu.bmp 
+Pixel rgb; 
+image.open(machupicchu.bmp) 
+bmp = image.toPixelMatrix(); 
+
 return 0; 
 } 
 // 
-prompt user to enter in image
-correct for user input error 
-use a loop to test all the images pixels and find the average value of the 3 components in the pixel then assign to that average value.
+//prompt user to enter in image
+//correct for user input error 
+//use a loop to test all the images pixels and find the average value of the 3 components in the pixel then assign to that average value.
